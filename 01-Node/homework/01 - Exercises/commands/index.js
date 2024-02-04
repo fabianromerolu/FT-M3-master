9af75942b975eb9a9d5e1f2af1fbf8320c305198
fabianromerolu <fabianromerolu@gmail.com>
2024-02-04 10:06:41 -0500

    HOMEWORK 1: DONE

--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,78 @@
 const fs = require("fs");
 const utils = require("../utils/request");
 const process = require("process");
+const { error } = require("console");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cmd());
+}
 
-function date() {}
+function date(print) {
+    print(Date());
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args);
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir(' ', (err, files) => {
+        if(err){
+            throw err;
+        }else{
+            print(files.join(' '));
+        }
+    });
+}
 
-function cat() {}
+function cat(print, args) {
+    fs.readFile(args, 'UTF-8', (error, data) => {
+        if(error){
+            throw new Error(error);
+        }else{
+            print(data);
+        }
+    });
+}
 
-function head() {}
+function head(print, args) {
+    fs.readFile(args, 'UTF-8', (error, data) => {
+        if(error){
+            throw new Error(error);
+        }else{
+            print(data.split('\n')[0]);
+        }
+    });
+}
 
-function tail() {}
+function tail(print, args) {
+    fs.readFile(args, 'UTF-8', (error, data) => {
+        if(error){
+            throw new Error(error);
+        }else{
+            arrayData = data.split('\n');
+            print(arrayData[arrayData.length - 1].trim());
+        }
+    });
+}
 
-function curl() {}
+function curl(print, args) {
+    utils.request(args, (error, response)=>{
+        if(error){
+            throw error;
+        }else{
+            print(response);
+        }
+    })
+}
 
-module.exports = {};
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl,
+};
